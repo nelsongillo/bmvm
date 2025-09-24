02ba585806b3ecec91e145de42513ea0b9b64e07
nelsongillo <nelson@gillos.de>
2025-09-24 18:07:59 +0200

    feat(benchy): native with args

--- mircobuild.sh
+++ mircobuild.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
-benches=("kvm" "load" "parse" "link" "run")
-links=("links1" "links8" "link16" "links32" "links64" "links128")
-output="bench/binaries/micro"
-mkdir -p "$output"
+micros=$1
+binaries=$2
+
+HOSTS=()
+# Find all micro files
+while IFS= read -r -d $'\0' file; do
+    HOSTS+=("$file")
+done < <(find "$micros" -maxdepth 1 -type f -executable -print0)
+
+GUESTS=()
+# Find all guests files
+while IFS= read -r -d $'\0' file; do
+    GUESTS+=("$file")
+done < <(find "$binaries" -maxdepth 1 -type f -executable -print0)
 
 # Loop over all combinations
-for b in "${benches[@]}"; do
-  for l in "${links[@]}"; do
-    cargo build --package micro --release --features="$b,$l"
-    cp ./target/release/micro "$output/micro-$b-$l"
+for h in "${HOSTS[@]}"; do
+  for g in "${GUESTS[@]}"; do
+        # Extract last 5 chars
+        suff_h="${h: -5}"
+        suff_g="${g: -5}"
+
+        if [[ "$suff_h" == "$suff_g" ]]; then
+          ./target/release/benchy --runtime native --mode exec --warmup 128 --iters 1024 --output ./data --file "$h" --args "$g"
+        fi
   done
 done
